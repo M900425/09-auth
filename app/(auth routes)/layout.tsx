a4c9f7774d8e869324fa92762e92cb48c585117f
M900425 <ifsubdtj@gmail.com>
2025-11-09 16:23:53 +0200

    fix

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,15 +1,13 @@
 import { ReactNode } from "react";
 import AuthProvider from "../../components/AuthProvider/AuthProvider";
 import TanStackProvider from "../../components/TanStackProvider/TanStackProvider";
-
-interface PrivateLayoutProps {
+interface AuthLayoutProps {
   children: ReactNode;
 }
-
-export default function PrivateLayout({ children }: PrivateLayoutProps) {
+export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
     <TanStackProvider>
-      <AuthProvider>{children}</AuthProvider>
+      <AuthProvider> {children} </AuthProvider>
     </TanStackProvider>
   );
 }
